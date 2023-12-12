d871baef459156b7c92810037baf067a489338a5
Yan0613 <1020303879@qq.com>
2023-12-12 20:04:13 +0800

    Initial commit

---
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+//记录实体类型
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
