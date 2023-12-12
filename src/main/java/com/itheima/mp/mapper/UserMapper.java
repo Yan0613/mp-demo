74ffe48496cf72059bfe8701d301a0ba541e1357
Yan0613 <1020303879@qq.com>
2023-12-12 20:14:11 +0800

    replace query, delete, insert, update

--- src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package com.itheima.mp.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 //记录实体类型
 public interface UserMapper extends BaseMapper<User> {
 
